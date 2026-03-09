56120729ceec1a363ec55f3919e716576cd5104b
Jovan Clue <jovan.clue@bison.howard.edu>
2026-03-09 06:00:55 -0400

    Started working on cipher, accepts input but hasn't fully implemented cipher functioning yet

--- cipher.py
+++ cipher.py
@@ -1,2 +1,18 @@
 import sys
 
+shift_amt = int(sys.argv[1])
+
+message = []
+for line in sys.stdin:
+	line = line.upper()
+	changed_line = []
+	for char in line:
+		if char.isalpha():
+			changed_line.append(char)
+		line = "".join(changed_line)
+	message.append(line)
+
+for line in message:
+	for char in line:
+		char += shift_amt	
+
